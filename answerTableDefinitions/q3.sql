6718894790ef405432e0d65bed2c9d8fbff1b562
Jerry Zheng <jerry.zheng@mail.utoronto.ca>
2018-03-11 17:32:41 -0400

    fixed a dublicated attribute name

--- answerTableDefinitions/q3.sql
+++ answerTableDefinitions/q3.sql
@@ -29,8 +29,8 @@ CREATE VIEW country_cycle as
 
 DROP VIEW IF EXISTS both_e_dates CASCADE;
 CREATE VIEW both_e_dates as
-  select p1.id, p1.country_id, p1.e_date, p1.previous_parliament_election_id,
-  p2.edate, country_cycle.name, country_cycle.election_cycle
+  select p1.id, p1.country_id, p1.e_date as curr_e_date, p1.previous_parliament_election_id,
+  p2.e_date as prev_e_date, country_cycle.name, country_cycle.election_cycle
   from parlamentary_elections as p1 join parlamentary_elections as p2
   on p1.previous_parliament_election_id = p2.id
   join country_cycle
