71e9e9a0aaa75976fcb9e5d2331eac982b8a3792
Jerry Zheng <jerry.zheng@mail.utoronto.ca>
2018-03-12 14:28:03 -0400

    added election_winners_sum_votes

--- answerTableDefinitions/q5.sql
+++ answerTableDefinitions/q5.sql
@@ -18,7 +18,7 @@ closeRunnerUp VARCHAR(100)
 DROP VIEW IF EXISTS fixed_nulls;
 CREATE VIEW fixed_nulls as
   select id, election_id, party_id, id as alliance_id, seats, votes
-  from relection_result
+  from election_result
   where alliance_id is NULL;
 
 DROP VIEW IF EXISTS election_result_no_null;
@@ -31,11 +31,57 @@ DROP VIEW IF EXISTS election_results_better;
 CREATE VIEW election_results_better as
   (select * from fixed_nulls) UNION (select * from election_result_no_null);
 
-DROP VIEW IF EXISTS alliances_sum;
-CREATE VIEW alliances_sum as
-  select election_id,  alliance_id, SUM(seats), SUM(votes)
-  from election_results_better
-  group by election_id, alliance_id;
+DROP VIEW IF EXISTS election_winners CASCADE;
+CREATE VIEW election_winners as
+    select parliamentary_elections.id as election_id, cabinet_party.party_id
+    from elections join cabinet
+        on parliamentary_elections.id = cabinet.election_id
+    join cabinet_party
+        on cabinet.id = cabinet_party.cabinet_id
+    where cabinet_party.pm = true;
+
+DROP VIEW IF EXISTS election_winners_unique CASCADE;
+CREATE VIEw election_winners_unique as
+    select DISTINCT election_id, party_id from election_winners;
+
+DROP VIEW IF EXISTS election_winners_result CASCADE;
+CREATE VIEw election_winners_result as
+    select id, election_result.election_id, election_resutl.party_id, id as alliance_id, seats, votes
+    from election_winners_unique join election_result
+    on election_winners_unique.election_id = election_result.election_id
+    and election_winners_unique.party_id = election_result.party_id;
+
+DROP VIEW IF EXISTS election_winners_fixed_nulls;
+CREATE VIEW election_winners_fixed_nulls as
+  select id, election_id, party_id, id as alliance_id, seats, votes
+  from election_winners_result
+  where alliance_id is NULL;
+
+DROP VIEW IF EXISTS election_winners_result_no_null;
+CREATE VIEW election_winners_result_no_null as
+  select id, election_id, party_id, alliance_id, seats, votes
+  from election_winners_result
+  where alliance_id is not NULL;
+
+DROP VIEW IF EXISTS election_winners_results_better;
+CREATE VIEW election_winners_results_better as
+  (select * from election_winners_fixed_nulls) UNION (select * from election_winners_result_no_null);
+
+DROP VIEW IF EXISTS election_winners_sum_votes;
+CREATE VIEW election_winners_sum_votes;
+    select election_id,  alliance_id, SUM(votes),
+    from election_winners_results_better
+    group by election_id, alliance_id;
+
+DROP VIEW IF EXISTS alliances_sum_votes;
+CREATE VIEW alliances_sum_votes as
+    select election_id,  alliance_id, SUM(votes),
+    from election_results_better
+    group by election_id, alliance_id;
+
+DROP VIEW IF EXISTS alliances_sum_votes_no_winners;
+CREATE VIEW alliances_sum_votes_no_winners as
+  (alliances_sum_votes) except (election_winners_sum_votes);
 
 -- the answer to the query
 --insert into q5
