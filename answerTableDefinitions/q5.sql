c2bf78a9f237a5a1b49aab50d36f7cbcd91f4621
Jerry Zheng <jerry.zheng@mail.utoronto.ca>
2018-03-12 17:25:00 -0400

    fixed typo

--- answerTableDefinitions/q5.sql
+++ answerTableDefinitions/q5.sql
@@ -23,7 +23,7 @@ CREATE VIEW fixed_nulls as
 
 DROP VIEW IF EXISTS election_result_no_null CASCADE;
 CREATE VIEW election_result_no_null as
-  select id, election_id, alliance_id, SUM(votes)
+  select election_id, alliance_id, SUM(votes)
   from election_result
   where alliance_id is not NULL
   group by election_id, alliance_id;
